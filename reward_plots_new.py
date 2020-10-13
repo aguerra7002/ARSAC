0fb40ef4dbfef59abdfd0419e2f44f0c20eaea7f
Alex Guerra <alexguerra618@gmail.com>
2020-10-13 14:08:56 -0700

    New reward plots for Batch size 256 experiments

--- reward_plots_new.py
+++ reward_plots_new.py
@@ -115,13 +115,14 @@ def plot_rewards(env_exp_dict, save_folder):
     plt.savefig(save_folder + name + '_rewards.pdf')
     plt.clf()
 
+# DM Control Transfer tasks
 walker_transfer_dict = {"SAC": ['21ed3d77773e4249b171a321a2bdcd07'],
                  "SAC w/ base transfer": ['2ce5802c9ea845fc9156514c21b53c02'], # Transferred from 0c00a35dea5e425d8af55d3804b3c7b0
                  "ARSAC": ['0e5f9791511242eaa54f175af46a42f7'],
                  "ARSAC w/ flow transfer": ['cf8a735331fc4fc689943cf1581f3c79'], # Transferred from 157216c90f8e400bab5264211ede1646
                  "ARSAC w/ base + flow transfer": ['a131e09fef224a27ab9d5050aa7d2dd5']} # Transferred from 157216c90f8e400bab5264211ede1646
 
-# DM Control Base Tests
+# DM Control Base Tests (Batch size 128, ARSAC-3)
 walker_walk_base_dict = {"SAC": ['5486ada760c640f7b5fbdd3680ce8258'],
                          "ARSAC-3": ['848a8336f98343dca7e09ff80e221dba']}
 walker_run_base_dict = {"SAC": ['a2277a87765e4ddd8d9af641a8ce97cf'],
@@ -134,6 +135,44 @@ hopper_stand_base_dict = {"SAC": ['27553ab85f5d4f2981339ecb244ed92a'],
                          "ARSAC-3": ['6fd32d97a7214344b16da9b6b6ae6d71']}
 hopper_hop_base_dict = {"SAC": ['e4c400fa2c654d739952138653271a55'],
                          "ARSAC-3": ['bcdbbdd174764aba84742d2f9122f789']}
+
+# DM Control Base Tests (Batch Size 256, ARSAC-5)
+walker_walk_base_dict2 = {"SAC": ['b1acc1d1812b490c91437d8735f37911', # 1
+                                  '25cabb2bf78b4a26847cdbf07fee1f73', # 2
+                                  '8671749406ad4e4a97372d36e28d1bd0'], # 3
+                         "ARSAC-5": ['3b7564c7ca044faeaf459e7aa5635b98',
+                                     '14007a1391ca4758a7c07250957902c6',
+                                     'c78c27f6b2854a38a9100a688c553b39']}
+walker_run_base_dict2 = {"SAC": ['3f271263d96b45009a19b32aa816260b',
+                                 'ac28eb5a38b3468b9de3c641ca3f86c2',
+                                 '5af66ac575284976a624b25371d99ef6'],
+                         "ARSAC-5": ['91cb40a43bb64e23854feb8efe76db8c',
+                                     '1441bfaa31954a239f0d3fa0cb789b1a',
+                                     '437f1e393c6e4ed5aced57583bda3460']}
+quadruped_walk_base_dict2 = {"SAC": ['ea66866a0f4440c4b5c9a979df987ae7',
+                                     'a7d9fc48ff7a49fc92faa6d83980ece1',
+                                     '475848cab7d24c4cab0bde5c7f4f24d3'],
+                         "ARSAC-5": ['68fd82b266614f3db1430cd7c63eaf7e',
+                                     '571019e4d63a46b6bab1069b150d3fa9',
+                                     '12b06528d71243bbb92516121cdbdf80']}
+quadruped_run_base_dict2 = {"SAC": ['af54967243e748cf94d62341712c8336',
+                                    '7d869b663a584665b2e7958342a44711',
+                                    'c32b0ffc37724d178efa886a1b94cfa6'],
+                         "ARSAC-5": ['6f9ba61406c54cefa9cdde52b2766e90',
+                                     '8c7010391eea4e0e94a0bcc900b1e550',
+                                     '6fe0d6da6c7247d0ae1e2c73959323c6']}
+hopper_stand_base_dict2 = {"SAC": ['c2eecc92b7034fab97c712a8d372e9a5',
+                                   '6cbfa580e0e349608eaead9286947edb',
+                                   'ef9f5a9f20e640d289c21a32c66b6dae'],
+                         "ARSAC-5": ['38a6cc65e68f4b9f93261d1a7fa5c13c',
+                                     '5ad134f56f92420cb0ea7c308e7cb324',
+                                     '9a082948d3cd4325808d1d48e9890c45']}
+hopper_hop_base_dict2 = {"SAC": ['84fe85ab190e46829a5ea83ce7f71c66',
+                                 '880a4442ab18430bb41d277e1da1d6a5',
+                                 '2c09f071cd51428c94af79294e80da1f'],
+                         "ARSAC-5": ['94b386e376124a1d8d157ce0a85c758a',
+                                     '497aaa6f7cc1481d89339c2a1df0bfd7',
+                                     '74404513a2a245c3adcff5b96b1c4254']}
 
 # DM Control Pixel Tests
 # TODO: FILLIN
@@ -148,13 +187,22 @@ to_plot_dict = {
         "Hopper Hop Base" : hopper_hop_base_dict
     }
 
+to_plot_dict2 = {
+        "Walker Walk Base 256" : walker_walk_base_dict2,
+        "Walker Run Base 256" : walker_run_base_dict2,
+        "Quadruped Walk Base 256" : quadruped_walk_base_dict2,
+        "Quadruped Run Base 256" : quadruped_run_base_dict2,
+        "Hopper Stand Base 256" : hopper_stand_base_dict2,
+        "Hopper Hop Base 256" : hopper_hop_base_dict2
+    }
+
 
 if __name__ == "__main__":
 
     # Specify the folder we want to save the visualizations to
     base_dir = "reward_plots_new/"
-    for env in to_plot_dict.keys():
-        env_exp_dict = to_plot_dict[env]
+    for env in to_plot_dict2.keys():
+        env_exp_dict = to_plot_dict2[env]
         print("Visualizing ", env)
 
         plot_rewards(env_exp_dict, base_dir)
